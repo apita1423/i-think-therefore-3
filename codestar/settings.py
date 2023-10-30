71f8324cd3c5f114c341ac17651c4868315e97b4
Amanda <apita1423@outlook.com>
2023-10-30 19:46:32 +0000

    Change allowed host

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://i-think-therefore-3-881a8fa8ae25.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['i-think-therefore-3-881a8fa8ae25.herokuapp.com']
 
 
 # Application definition
